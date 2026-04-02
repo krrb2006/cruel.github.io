e436b1d4e735de7dc2c54f2b91e0a8166ac796a6
KRRB2006 <2913137783@qq.com>
2026-04-03 02:06:10 +0800

    upgrade blog with dopamine theme and effects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://krrb2006.github.io',
   base: '/cruel.github.io',
---
+++ src/data/site.ts
@@ -0,0 +1,13 @@
+export const site = {
+  blogName: "jintong's",
+  nickname: "刘瑨同",
+  initials: "L",
+  role: "一如既往",
+  intro:
+    "Welcome to my world",
+  email: "2913137783@qq.com",
+  github: "https://github.com/krrb2006",
+  avatar: "avatar.jpg",
+  background: "bg.jpg",
+  tags: ["骄傲不死", "浪漫无悔", "死性不改", "学习傻子"],
+};
